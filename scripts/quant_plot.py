a29d3c38b5f4c41ac1cba42a06168796711aa759
Zbedd <89368092+Zbedd@users.noreply.github.com>
2025-06-18 14:51:32 -0600

    Allow filtering groups via config

--- scripts/quant_plot.py
+++ scripts/quant_plot.py
@@ -23,6 +23,11 @@ def main(cfg):
     csv_path = os.path.join(base_path, csv_rel_path)
         
     df = fiji_preprocess.preprocess_fiji_csv(csv_path)
+
+    groups = cfg.get('groups')
+    if groups:
+        df = df[df['group'].isin(groups)]
+
     df_collapsed_to_img = fiji_preprocess.collapse_fracIn(df)
     
     # Statistics
